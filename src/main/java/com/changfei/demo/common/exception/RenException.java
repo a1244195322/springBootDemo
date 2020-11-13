d665576d0ea922f4804633a1bd846065c6578fbc
a1244195322 <39880206+a1244195322@users.noreply.github.com>
2020-11-13 14:41:58 +0800

    Create RenException.java

---
+++ src/main/java/com/changfei/demo/common/exception/RenException.java
@@ -0,0 +1,10 @@
+package com.changfei.demo.common.exception;
+
+import com.changfei.demo.common.utils.MessageUtils;
+import lombok.Data;
+
+/**
+ * <b>标题：</b> <br>
+ * <b>描述：</b> <br>
+ * <b>设计：</b> Administrator<br>
+ * <b>创建：</b> 2020/
