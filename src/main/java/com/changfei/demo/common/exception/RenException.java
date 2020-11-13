d5f3c07f0273c9c49ca31fb12f20ebb165455e97
a1244195322 <39880206+a1244195322@users.noreply.github.com>
2020-11-13 14:43:29 +0800

    Update RenException.java

--- src/main/java/com/changfei/demo/common/exception/RenException.java
+++ src/main/java/com/changfei/demo/common/exception/RenException.java
@@ -7,4 +7,52 @@ import lombok.Data;
  * <b>标题：</b> <br>
  * <b>描述：</b> <br>
  * <b>设计：</b> Administrator<br>
- * <b>创建：</b> 2020/
+ * <b>创建：</b> 2020/11/12 11:35<br>
+ * <b>更新：</b>时间： 更新人： 更新内容：<br>
+ * <b>审查：</b>时间： 审查人： 审查情况：<br>
+ * <b>抽查：</b>时间： 抽查人： 抽查情况：
+ *
+ * @author Administrator
+ * @version 1.0
+ */
+@Data
+public class RenException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    private int code;
+    private String msg;
+
+    public RenException(int code) {
+        this.code = code;
+        this.msg = MessageUtils.getMessage(code);
+    }
+
+    public RenException(int code, String... params) {
+        this.code = code;
+        this.msg = MessageUtils.getMessage(code, params);
+    }
+
+    public RenException(int code, Throwable e) {
+        super(e);
+        this.code = code;
+        this.msg = MessageUtils.getMessage(code);
+    }
+
+    public RenException(int code, Throwable e, String... params) {
+        super(e);
+        this.code = code;
+        this.msg = MessageUtils.getMessage(code, params);
+    }
+
+    public RenException(String msg) {
+        super(msg);
+        this.code = ErrorCode.INTERNAL_SERVER_ERROR;
+        this.msg = msg;
+    }
+
+    public RenException(String msg, Throwable e) {
+        super(msg, e);
+        this.code = ErrorCode.INTERNAL_SERVER_ERROR;
+        this.msg = msg;
+    }
+}
